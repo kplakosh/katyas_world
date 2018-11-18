9604e4372d029da1c1368b80bd8ae260e9407860
kplakosh <kplakosh@gmail.com>
2018-11-17 21:45:16 -0800

    backdrop and modal logic added

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log(document.querySelector('.trip-image'));
+var imageSF = document.querySelector('.trip-image');
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+
+
+function openModal() {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+}
+
+openModal();
