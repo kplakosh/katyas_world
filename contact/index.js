ff836919ad351d1b23826703bd7bfff28cf32da9
kplakosh <kplakosh@gmail.com>
2018-11-17 23:25:20 -0800

    validation for fields added

--- contact/index.js
+++ contact/index.js
@@ -2,9 +2,11 @@ var inputName = document.getElementById('input-name');
 var inputMail = document.getElementById('input-mail');
 var inputMessage = document.getElementById('input-message');
 var button = document.querySelector('.submit-button');
+var regEx = /\S+@\S+\.\S+/;
 
 function checkInput() {
-    if (inputName.value.trim() !== "") {
+    if (inputName.value.trim() !== "" && regEx.test(inputMail.value) && 
+        inputMessage.value.trim() !== "") {
         button.style.backgroundColor = "#fa923f"
     } else {
         button.style.backgroundColor = "#979695";
